4d16cf4f9d9dc1cdc8fd3cad156b54e1576cbca6
Matheus <30qr4xnl@duck.com>
2024-09-03 23:47:08 -0300

    feature/lgpd-exclusion-api: ajustando validate tags dto

--- src/core/domain/dtos/lgpd_removal_request_dto.go
+++ src/core/domain/dtos/lgpd_removal_request_dto.go
@@ -1,8 +1,8 @@
 package dtos
 
 type LGPDRemovalRequestDto struct {
-	Name           string `json:"name" validate:"required, regexp=^[a-zA-ZÀ-ÿ'’\- ]{1,255}$"`
-	Address        string `json:"address" validate:"required, regexp=^[a-zA-ZÀ-ÿ'’\- ]{1,255}$"`
-	PhoneNumber    string `json:"phone_number" validate:"required, regexp=^\+?(\d{1,3})?[-.\s]?(\(?\d{2}\)?)?[-.\s]?\d{4,5}[-.\s]?\d{4}$"`
-	PaymentHistory bool   `json:"payment_history" validate:"required,oneof=true false"`
+	Name           string `json:"name" validate:"nonzero,min=10,max=255"`
+	Address        string `json:"address" validate:"nonzero,min=10,max=255"`
+	PhoneNumber    string `json:"phone_number" validate:"len=11"`
+	PaymentHistory bool   `json:"payment_history" validate:"nonnil"`
 }
